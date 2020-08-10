c842ea8d0df1a4ec11d2b80a80a46eed42ab07fa
Yoav Nativ <yoavnativ@gmail.com>
2020-08-10 14:57:46 -0400

    deploy to aws succesfully

---
+++ src/handler.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports.hello = async event => {
+    console.log('This is a test hello lambda console log');
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'AWS CI/CD Test - Your function executed successfully! 2:48'
+      },
+      null,
+      2
+    ),
+  };
+};
